253bbe582e6aa004bf600f3ef6fb8541a7eae4cb
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-03-14 11:17:54 -0400

    finished webpack practice

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import printMe from "./print";
+
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
+
   element.textContent = "Hello World!";
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
