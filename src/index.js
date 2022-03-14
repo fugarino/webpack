5d3718fec85e9949b113a2fe55601137f56cc7a4
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-03-14 09:58:51 -0400

    loading images

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import "./style.css";
+import Hero from "./hero.png";
 
 function component() {
   const element = document.createElement("div");
   element.textContent = "Hello World!";
   element.classList.add("hello");
+  const myHero = new Image();
+  myHero.src = Hero;
+  element.appendChild(myHero);
   return element;
 }
 
